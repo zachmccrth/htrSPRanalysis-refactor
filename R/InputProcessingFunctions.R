26f390ac97d32d13a73ca47cf1408218bb28fafd
Janice McCarthy <janice@dcc-chsi-03.rc.duke.edu>
2023-03-09 14:12:28 -0500

    fixed suppression not working

--- R/InputProcessingFunctions.R
+++ R/InputProcessingFunctions.R
@@ -35,12 +35,12 @@ process_sample_sheet <- function(sample_sheet){
 
   }
 
-  sample_sheet %>%
-    suppressWarnings(tidyr::separate(`Position/Channel/Sensor`, into = c("BeginPosition", "EndPosition", sep = "-"))) %>%
-    suppressWarnings(tidyr::separate(BeginPosition, into = c("Row", "Column"), sep = 1)) %>%
-    suppressWarnings(tidyr::separate(EndPosition, into = c("EndRow", "EndColumn"), sep = 1)) %>%
+  suppressWarnings(sample_sheet %>%
+    tidyr::separate(`Position/Channel/Sensor`, into = c("BeginPosition", "EndPosition", sep = "-")) %>%
+    tidyr::separate(BeginPosition, into = c("Row", "Column"), sep = 1) %>%
+    tidyr::separate(EndPosition, into = c("EndRow", "EndColumn"), sep = 1) %>%
     dplyr::mutate(Column = as.integer(Column)) %>%
-    dplyr::mutate(EndColumn = as.integer(EndColumn)) -> RowExpansionTemplate
+    dplyr::mutate(EndColumn = as.integer(EndColumn)) -> RowExpansionTemplate)
 
   length_ss <- dim(RowExpansionTemplate)[1]
   ss_exp <- NULL
